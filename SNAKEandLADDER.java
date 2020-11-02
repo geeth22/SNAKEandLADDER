d53b5abff8e558bcf2b3210e614d90d5e3e44e01
geeth22 <geethchandra22@gmail.com>
2020-11-02 07:14:49 +0530

    Player checks for a option No Play, Snake and Ladder

--- SNAKEandLADDER.java
+++ SNAKEandLADDER.java
@@ -1,17 +1,42 @@
 public class SnakeAndLadder{
 	
-	Random rand = new Random();
-	
-	public static void main(String args[]) {
-		
-		SnakeAndLadder obj=new SnakeAndLadder();
-		System.out.println("Dice value : "+obj.DiceRoll());
+	int CurrentPosition = 0;
+	int DiceResult = 0;
+
+	public static void main(String args[]){
+		SnakeAndLadder obj = new SnakeAndLadder();
+		System.out.println("Player Position : "+ obj.CheckOptions());
 	}
-	
-	public int DiceRoll(){
-		int DiceRoll = (rand.nextInt(6))+1;
+
+	public int DiceRoll(int RollMax){
+		Random rand = new Random();
+		int DiceRoll = (rand.nextInt(RollMax)) + 1;
 		return DiceRoll;
 	}
 
-}
+	public int CheckOptions(){
 
+		int CheckOptionsForPlay = DiceRoll(3);
+
+		switch(CheckOptionsForPlay){
+
+         	case 1:
+            		System.out.println("No play");
+         	break;
+
+         	case 2:
+            		System.out.println("You got Snake");
+            		DiceResult=DiceRoll(6);
+            		CurrentPosition -= DiceResult;
+         	break;
+
+         	case 3:
+            		System.out.println("You got Ladder");
+            		DiceResult = DiceRoll(6);
+            		CurrentPosition += DiceResult;
+         	break;
+
+      		}
+      	return CurrentPosition;
+	}
+}
