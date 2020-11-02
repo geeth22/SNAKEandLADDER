79f47cc4f7fc975c8168c5d2546e15584469aba2
geeth22 <geethchandra22@gmail.com>
2020-11-02 08:31:12 +0530

    Play the game with two players. In case of player gets a ladder, then plays again. Finally Report which player won the game

--- SNAKEandLADDER.java
+++ SNAKEandLADDER.java
@@ -1,66 +1,83 @@
+import java.util.Random;
+
 public class SnakeAndLadder{
-	
-	int CurrentPosition = 0;
-	int DiceResult = 0;
-	int DiceRollCount = 0;
-	int WinningPosition = 100;
-	
-	public static void main(String args[]){
-		SnakeAndLadder obj = new SnakeAndLadder();
-		System.out.println("Player Position : "+ obj.CheckOptions());
-	}
 
-	public int DiceRoll(int RollMax){
-		Random rand = new Random();
-		int DiceRoll = (rand.nextInt(RollMax)) + 1;
-		DiceRollCount++;
-		return DiceRoll;
-	}
+    int Player1CurrentPosition;
+    int Player2CurrentPosition;
+    int DiceResult = 0;
+    public static final int WinningPosition = 100; 
 
-	public int CheckOptions(){
-		
-		while(CurrentPosition < WinningPosition){
-		int CheckOptionsForPlay = DiceRoll(3);
+    public static void main(String args[]){
 
-		switch(CheckOptionsForPlay){
+        SnakeAndLadder obj = new SnakeAndLadder();
 
-         	case 1:
-            		System.out.println("\nNo play and Current Position is " + CurrentPosition);
-         	break;
+        while (obj.Player1Position() < 100 && obj.Player2Position() < 100){
+            obj.Player1Position();
+            obj.Player2Position();
+        }
+    }
 
-         	case 2:
-            		DiceResult = DiceRoll(6);
+    public int Player1Position(){
 
-			if(CurrentPosition <= DiceResult){
-                  	CurrentPosition = 0;
-                  	System.out.println("\nYou got Snake with dice face of " + DiceResult + " and Current Position is " + CurrentPosition);
-               		}
- 
-              		else{
-                  	CurrentPosition -= DiceResult;
-                  	System.out.println("\nYou got Snake with dice face of " + DiceResult + " and Current Position is " + CurrentPosition);
-               		}
-         	break;
+        Player1CurrentPosition = CheckOptions(Player1CurrentPosition,1);
+        return Player1CurrentPosition;
+    }
 
-         	case 3:
-            		DiceResult = DiceRoll(6);
+    public int Player2Position(){
+        Player2CurrentPosition = CheckOptions(Player2CurrentPosition,2);
+        return Player2CurrentPosition;
+    }
 
-                    if(CurrentPosition + DiceResult > WinningPosition){
+    public int DiceRoll(int RollMax){
+        Random rand = new Random();
+        int DiceRoll = (rand.nextInt(RollMax)) + 1;
+        return DiceRoll;
+    }
 
-                        while(CurrentPosition + DiceResult == WinningPosition){
+    public int CheckOptions(int CurrentPosition, int Player) {
+
+            int CheckOptionsForPlay = DiceRoll(3);
+
+            switch (CheckOptionsForPlay) {
+                
+                case 1:
+                    System.out.println("\nPlayer "+ Player + " No play and Current Position is " + CurrentPosition);
+                break;
+
+                case 2:
+                    DiceResult = DiceRoll(6);
+
+                    if (CurrentPosition <= DiceResult) {
+                        CurrentPosition = 0;
+                        System.out.println("\nPlayer "+ Player + " got Snake with dice face of " + DiceResult + " and Current Position is " + CurrentPosition);
+                    } else {
+                        CurrentPosition -= DiceResult;
+                        System.out.println("\nPlayer "+ Player + " You got Snake with dice face of " + DiceResult + " and Current Position is " + CurrentPosition);
+                    }
+                break;
+
+                case 3:
+                    DiceResult = DiceRoll(6);
+
+                    if (CurrentPosition + DiceResult > WinningPosition) {
+
+                        while (CurrentPosition + DiceResult == WinningPosition) {
                             DiceResult = DiceRoll(6);
                             CurrentPosition += DiceResult;
                         }
                         break;
-                    }
-                    else {
+                    } else {
                         CurrentPosition += DiceResult;
-                        System.out.println("\nYou got Ladder with dice face of " + DiceResult + " and Current Position is " + CurrentPosition);
-            break;
-		 }
-      		}
-	}
-	System.out.println("\nNumber of times dice was rolled to win the game"+DiceRollCount);
-      return CurrentPosition;
-   }
+                        System.out.println("\nPlayer " + Player +" got Ladder with dice face of " + DiceResult + " and Current Position is " + CurrentPosition);
+
+                        //Checking Each Player's position which should be below Winning Position
+                        if(CurrentPosition == WinningPosition){
+                            System.out.println("\nPlayer "+ Player + " won");
+                        }
+                        break;
+                    }
+            }
+        return CurrentPosition;
+
+    }
 }
